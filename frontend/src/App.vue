9614b8cc8d5d3358527e54f21a757db9e724a55d
Hormone4 <117640003+Hormone4@users.noreply.github.com>
2025-04-07 11:08:42 +0200

    added button global style
    + indent correction

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,15 +20,33 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-.zoom-hover:hover {
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  .zoom-hover:hover {
     transform: scale(1.1);
     transition: transform 0.1s;
+  }
+
+  /* button OR input[type="button"] */
+  button, input[type="button"] {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 9px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 4px;
+  }
+  button:hover, input[type="button"]:hover {
+    background-color: #45a049;
   }
 </style>
